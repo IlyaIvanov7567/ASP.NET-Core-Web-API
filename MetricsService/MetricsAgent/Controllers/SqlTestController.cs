b5d160412d64bb18ccd77bc7e556e0ea3f88f66a
IlyaIvanov <ilyaivanov7567@gmail.com>
2022-01-15 14:59:28 +0300

    SqlTestController fixed

--- MetricsService/MetricsAgent/Controllers/SqlTestController.cs
+++ MetricsService/MetricsAgent/Controllers/SqlTestController.cs
@@ -85,7 +85,7 @@ namespace MetricsAgent.Controllers
                             {
                                 Id = reader.GetInt32(0), // читаем данные полученные из базы данных
                                 Value = reader.GetInt32(1), // преобразуя к целочисленному типу
-                                Time = (TimeSpan)reader.GetValue(2)
+                                Time = TimeSpan.FromTicks(reader.GetInt64(2))
                             };
                             // увеличиваем значение счетчика
                             counter++;
